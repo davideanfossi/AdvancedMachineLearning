4b54a2ece8c462cf3c5d0093a87a40f4034cdadd
Nicola Gutierrez <nicolagutierrez.ng@gmail.com>
2024-01-19 00:02:18 +0100

    test: debug on reshape of data in save_feat

--- save_feat.py
+++ save_feat.py
@@ -93,12 +93,19 @@ def save_feat(model, loader, device, it, num_classes):
     with torch.no_grad():
         for i_val, (data, label, video_name, uid) in enumerate(loader):
             label = label.to(device)
+            
 
             for m in modalities:
                 batch, _, height, width = data[m].shape
-                data[m] = data[m].reshape(batch, args.save.num_clips,
-                                          args.save.num_frames_per_clip[m], -1, height, width)
+                print(f"Before Reshape - {m}: {data[m].shape}")
+
+                data[m] = data[m].reshape(batch, args.save.num_clips, args.save.num_frames_per_clip[m], -1, height, width)
                 data[m] = data[m].permute(1, 0, 3, 2, 4, 5)
+                #data[m] = data[m].reshape(batch, args.save.num_clips,
+                #                          args.save.num_frames_per_clip[m], -1, height, width)
+                #data[m] = data[m].permute(1, 0, 3, 2, 4, 5)
+
+                print(f"After Reshape - {m}: {data[m].shape}")
 
                 logits[m] = torch.zeros((args.save.num_clips, batch, num_classes)).to(device)
                 features[m] = torch.zeros((args.save.num_clips, batch, model.task_models[m]
