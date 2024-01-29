496f745c55994937c40fcc0ec0373044c76f9a7f
Nicola Gutierrez <nicolagutierrez.ng@gmail.com>
2024-01-29 11:42:06 +0100

    feat: adding mediana and max, min

--- train_val/check_len_records.py
+++ train_val/check_len_records.py
@@ -1,20 +1,28 @@
 import pandas as pd
+import statistics
+
 
 def extract_infos():
-  df = pd.read_pickle('D1_test.pkl')
-  lens = []
-  avg = sum = 0
+    df = pd.read_pickle("D1_test.pkl")
+    lens = []
+    avg = sum = 0
 
-  for record in df.iterrows():
-      len = record[1].iloc[7] - record[1].iloc[6]
-      sum += len
-      avg += 1
-      lens.append(len)
+    for record in df.iterrows():
+        len = record[1].iloc[7] - record[1].iloc[6]
+        sum += len
+        avg += 1
+        lens.append(len)
 
-  avg = round(sum / avg)
+    avg = round(sum / avg)
+    mediana = statistics.median(lens)
+    min_value = min(lens)
+    max_value = max(lens)
 
-  with open('D1_test_len.txt', 'w') as f:
-      f.write(f'sum: {sum}\navg: {avg}\n')
+    with open("D1_test_len.txt", "w") as f:
+        f.write(
+            f"sum: {sum}\navg: {avg}\nmediana: {mediana}\nmax: {max_value}\nmin: {min_value}\n"
+        )
 
-if __name__ == '__main__':
-  extract_infos()
+
+if __name__ == "__main__":
+    extract_infos()
