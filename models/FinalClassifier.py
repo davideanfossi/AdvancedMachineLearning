42cb62b3ba95955c068c76ea3b7dd46097bdf53c
Nicola Gutierrez <nicolagutierrez.ng@gmail.com>
2024-02-03 20:28:27 +0100

    chore: adjust setup for colab

--- models/FinalClassifier.py
+++ models/FinalClassifier.py
@@ -1,12 +1,20 @@
 from torch import nn
 
 
-class Classifier(nn.Module):
-    def __init__(self, num_classes):
-        super().__init__()
+class TransformerClassifier(nn.Module):
+    def __init__(self, input_size, hidden_size, num_classes):
+        super(TransformerClassifier, self).__init__()
         """
         [TODO]: the classifier should be implemented by the students and different variations of it can be tested
         in order to understand which is the most performing one """
 
+        self.fc = nn.Linear(input_size, hidden_size)
+        self.relu = nn.ReLU()
+        self.classifier = nn.Linear(hidden_size, num_classes)
+
     def forward(self, x):
-        return self.classifier(x), {}
+        y = self.fc(x)
+        y = self.relu(x)
+        y = self.classifier(x)
+        return x, {}
+
