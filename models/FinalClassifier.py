27b4d3734efc3db32d1b7af694659d3f0b2de923
Nicola Gutierrez <nicolagutierrez.ng@gmail.com>
2024-02-05 00:21:46 +0100

    feat: transformer classifier

--- models/FinalClassifier.py
+++ models/FinalClassifier.py
@@ -1,38 +1,39 @@
 import torch
 from torch import nn
-
-# For general data processing
-import numpy as np
-import pandas as pd
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay
-from sklearn.metrics import precision_recall_fscore_support as score
-import matplotlib.pyplot as plt
-
-# to convert to dataset datatype - the transformers library does not work well with pandas
+from torch.nn import TransformerEncoder, TransformerEncoderLayer
 from utils.logger import logger
-from torch.utils.data import Dataset
 
 
 class TransformerClassifier(nn.Module):
-    def __init__(self, input_dim, hidden_dim, num_heads, num_layers):
+    def __init__(
+        self, num_classes, seq_length=1024, num_heads=8, hidden_size=512, num_layers=6
+    ):
         super(TransformerClassifier, self).__init__()
-
-        self.embedding = nn.Embedding(input_dim, hidden_dim)
-        self.transformer_encoder = nn.TransformerEncoder(
-            nn.TransformerEncoderLayer(
-                d_model=hidden_dim,
-                nhead=num_heads,
-            ),
-            num_layers=num_layers
-        )
-
-        self.fc = nn.Linear(hidden_dim, 1)
+        self.embedding = nn.Linear(
+            seq_length, hidden_size
+        )  # Use a Linear layer for embedding
+        encoder_layers = TransformerEncoderLayer(hidden_size, num_heads)
+        self.transformer_encoder = TransformerEncoder(encoder_layers, num_layers)
+        self.fc = nn.Linear(hidden_size, num_classes)
 
     def forward(self, x):
-        x = self.embedding(x)
-        x = x.permute(1, 0, 2)  # Change the shape for transformer input
-        x = self.transformer_encoder(x)
-        x = x.mean(dim=0)  # Aggregate the transformer output
-        x = self.fc(x)
-        return x.squeeze(1)
+        x = self.embedding(x)  # Shape: (batch_size, seq_length, hidden_size)
+        x = x.unsqueeze(1)
+        x = x.permute(1, 0, 2)  # Permute to (seq_length, batch_size, hidden_size)
+
+        # Forward pass through transformer encoder
+        transformer_output = self.transformer_encoder(x)
+
+        # Extract features and compute logits -> Taking output of the first token as features
+        feat = transformer_output[0, :, :]
+        logger.info(f"\nSTART TESTING...")
+        logger.info(f"FEAT: {feat}")
+        logger.info(f"FEAT SHAPE: {feat.shape}")
+
+        # Compute logits
+        logits = self.fc(feat)  # Fully connected layer for logits
+        logger.info(f"LOGITS: {logits}")
+        logger.info(f"LOGITS SHAPE: {logits.shape}")
+        logger.info(f"END TESTING...\n")
+
+        return logits, {"features": feat}
