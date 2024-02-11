c0bcd76c57c47ed3e3d53458164479b44afa90df
Nicola Gutierrez <nicolagutierrez.ng@gmail.com>
2024-02-11 16:28:37 +0100

    feat: adding emg extraction ToDo: should be test

--- tester.py
+++ tester.py
@@ -1,6 +1,8 @@
 import numpy as np
+import sys
 from utils.temporal_aggregation import aggregate_features
 from utils.extract_pkl import *
+from utils.extract_hdf5 import *
 
 pkl_folder = [
     "saved_features/aggregated_test_D5",
@@ -11,14 +13,39 @@ pkl_folder = [
     "saved_features/aggregated_test_U25",
 ]
 
+pkl_folder_action_net = ["action-net/ActionNet_test", "action-net/ActionNet_train"]
+
+hdf5_filepath = "../data/action_net/EMG/ActionNet Wearables S04.hdf5"
+hdf5_out_folder = "../data/action_net/EMG/view"
+
 if __name__ == "__main__":
-    aggregate_features("train")
-    """
-    extract_pkl(pkl_folder)
+    # FEATURE EXTRACTION
+    # aggregate_features("train")
+    # extract_pkl(pkl_folder)
 
-    data = np.array((get_data_from_pkl("saved_features/test_D5"))['features'])
-    data_aggregated = np.array((get_data_from_pkl("saved_features/aggregated_test_D5"))['features'])
+    # HDF5 handler
+    keys = [
+        "in_path",
+        "out_folder",
+        "mode",
+        "device",
+        "stream",
+    ]
 
-    print(f"\nData shape: {data.shape}")
-    print(f"Data aggregated shape: {data_aggregated.shape}")
-    """
+    params = {
+        "in_path": hdf5_filepath,
+        "out_folder": hdf5_out_folder,  # --> offset +2
+    }
+
+    # ? Passing parameters with this setup:
+    # python tester.py <save> <in_path> <out_folder> <mode> <device> <stream>
+
+    # ? offset used to avoid passing all parameters
+    offset = 1 + len(params)
+
+    if len(sys.argv) > 1:
+        for i in range(offset, len(sys.argv)):
+            params[keys[i - offset + 1]] = sys.argv[i]
+
+    hdf5_extractor = HDF5Extractor(*params.values())
+    hdf5_extractor.extract_hdf5(save=False)
