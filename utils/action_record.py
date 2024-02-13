276e71103e02e489be8ec52707eb5c292e52c336
Emilio Joseph Grieco s317869 <s317869@studenti.polito.it>
2024-02-13 12:19:25 +0100

    feat: actionEMGRecord class

---
+++ utils/action_record.py
@@ -0,0 +1,26 @@
+
+
+class ActionEMGRecord(object):
+    def __init__(self, tup, dataset_conf):
+        #self._index = str(tup[0])
+        self._series = tup[1]       #forse vuole tup[0]
+        self.dataset_conf = dataset_conf
+
+    @property
+    def id(self):
+        return self._series['id']
+
+    @property
+    def myo_right_readings(self):
+        return self._series['myo_right_readings']
+
+    @property
+    def myo_left_readings(self):
+        return self._series['myo_left_readings']
+
+
+    @property
+    def label(self):
+        if 'verb' not in self._series.keys().tolist():
+            raise NotImplementedError
+        return self._series['verb']
